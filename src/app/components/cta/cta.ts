cd743473ffdcede162338a3d38f48e44fbe100d4
liviaghirardi <contato.liviaghirardi@gmail.com>
2026-08-20 21:50:50 -0300

    chore: initial angular project structure and setup

---
+++ src/app/components/cta/cta.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-cta',
+  styleUrl: './cta.css',
+  templateUrl: './cta.html',
+})
+export class Cta {}
---
+++ src/app/components/features/features.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-features',
+  styleUrl: './features.css',
+  templateUrl: './features.html',
+})
+export class Features {}
